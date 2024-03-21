78560e0e717f81c120103c7ad4471eb20f9a3002
andreasdan <andreas.petersen01@gmail.com>
2024-03-21 20:40:08 +0100

    improvements to training and changed object detection to object tracking

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 from ultralytics import YOLO
-from ultralytics.utils.plotting import Annotator
 import cv2
 
-model = YOLO("runs/detect/train10/weights/best.pt")
-cap = cv2.VideoCapture("vid/Bane_different_orientation.mp4")
+model = YOLO("model/best.pt")
+cap = cv2.VideoCapture(0)
 
-threshold = 0.70
-
-ret = True
+ret, frame = cap.read()
 while ret:
-    ret, frame = cap.read()
 
-    results = model.predict(frame)
+    results = model.track(frame, persist=True)
+    frame_ = results[0].plot()
+    cv2.imshow("YOLO", frame_)
 
-    for result in results:
-        annotator = Annotator(frame)
-        boxes = result.boxes
-        for box in boxes:
-            cords = box.xyxy[0]
-            cls = box.cls
-            conf = box.conf
-
-            if conf >= threshold:
-                label = f"{model.names[int(cls)]} {float(conf):0.2f}"
-                annotator.box_label(cords, label)
-
-    img = annotator.result()
-    cv2.imshow("YOLO", img)
-
+    # Press q to stop
     if cv2.waitKey(1) == ord('q'):
         break
+
+    ret, frame = cap.read()
 
 cap.release()
 cv2.destroyAllWindows()
--- training.py
+++ training.py
@@ -1,7 +1,14 @@
 from ultralytics import YOLO
+import torch
 
+# Model specific for training small objects
 model = YOLO("yolov8s-p2.yaml").load("yolov8s.pt")
 
 if __name__ == "__main__":
-    # Note that device=0 only works with CUDA cores
-    model.train(data="data/data.yaml", epochs=6, imgsz=1000, device=0, batch=-1)
+
+    device = "cpu"
+    # use CUDA cores when available
+    if torch.cuda.is_available():
+        device = 0
+
+    model.train(data="data/data.yaml", epochs=6, imgsz=1024, device=device, batch=-1)
