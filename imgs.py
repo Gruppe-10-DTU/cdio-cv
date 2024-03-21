9f3e8824111c5a972a10dc124ba957a984ec868b
andreasdan <andreas.petersen01@gmail.com>
2024-03-21 21:27:06 +0100

    added script for image saving from video

---
+++ imgs.py
@@ -0,0 +1,18 @@
+# Saves an image for every frame in a given video capture (use with CVAT annotations)
+
+import cv2
+
+cap = cv2.VideoCapture("")
+count = 0
+max_count = 0
+
+ret, frame = cap.read()
+
+while ret and max_count >= count:
+    cv2.imwrite(f"img/frame_{str(count).zfill(6)}.jpg", frame)
+    count += 1
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+    ret, frame = cap.read()
