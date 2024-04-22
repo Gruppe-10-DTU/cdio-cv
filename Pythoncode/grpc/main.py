f8863154aa1aba76a96e95c509128bb40d145ab8
sand4570 <s224323@student.dtu.dk>
2024-04-22 20:27:16 +0200

    Turn and backwards call

--- Pythoncode/grpc/main.py
+++ Pythoncode/grpc/main.py
@@ -1,14 +1,31 @@
 import grpc
+import time
 
 import protobuf_pb2
 import protobuf_pb2_grpc
 
+
 def run():
-    # Go forward testing
+
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = protobuf_pb2_grpc.RobotStub(channel)
+
+        # Go forward testing
         response = stub.Move(protobuf_pb2.MoveRequest(direction="1", distance=2, speed=3))
         print(response)
+
+        time.sleep(3)
+
+        # backwards
+        response = stub.Move(protobuf_pb2.MoveRequest(direction="-1", distance=2, speed=3))
+        print(response)
+
+        time.sleep(3)
+
+        # Turn
+        response = stub.Turn(protobuf_pb2.TurnRequest(direction="1", degrees=2))
+        print(response)
+
 
 if __name__ == "__main__":
     run()
