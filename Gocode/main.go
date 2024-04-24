abcd68b47707a6ac480d5979d795d7fe06e21c09
Philip Cramer <s224319@dtu.dk>
2024-04-24 14:01:36 +0200

    created borMotorsRunning function

--- Gocode/main.go
+++ Gocode/main.go
@@ -293,3 +293,18 @@ func resetGyros() {
 	gyro2.SetPollRate(5 * time.Millisecond)
 
 }
+func bothMotorsRunning() bool {
+	leftMotor, err := ev3dev.TachoMotorFor("ev3-ports:outA", "lego-ev3-l-motor")
+	if err != nil {
+		return false
+	}
+
+	rightMotor, err := ev3dev.TachoMotorFor("ev3-ports:outD", "lego-ev3-l-motor")
+	if err != nil {
+		return false
+	}
+	leftRunning, _ := leftMotor.State()
+	rightRunning, _ := rightMotor.State()
+	fmt.Printf("left: %s\tright: %s\n", leftRunning, rightRunning)
+	return (leftRunning == ev3dev.Running || leftRunning == ev3dev.Ramping) && (rightRunning == ev3dev.Running || rightRunning == ev3dev.Ramping)
+}
