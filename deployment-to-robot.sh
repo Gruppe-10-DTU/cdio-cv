7fb8ba2f748860f2045ea0f0379d12ad65da1794
Philip Cramer <s224319@dtu.dk>
2024-04-24 09:36:55 +0200

    now stops and starts grpc service when deploying

--- deployment-to-robot.sh
+++ deployment-to-robot.sh
@@ -12,7 +12,7 @@ protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=p
 cd ..
 GOOS=linux GOARCH=arm GOARM=5 go build -o robot-grpc
 
-#ssh robot@$1 "sudo systemctl stop robot.service" || exit
+ssh robot@$1 "sudo systemctl stop robot.service" || exit
 scp ./robot-grpc robot@$1:~/robot-grpc || exit
-#ssh robot@$1 "sudo systemctl start robot.service"
+ssh robot@$1 "sudo systemctl start robot.service"
 
