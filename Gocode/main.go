7ff9d56ae0aee373079c7e76efb82b9f6d1c7636
Philip Cramer <s224319@dtu.dk>
2024-05-01 10:24:48 +0200

    robot now handles if one gyro is present or if one of them fails to connect

--- Gocode/main.go
+++ Gocode/main.go
@@ -235,12 +235,18 @@ func (s *robotServer) Stats(_ context.Context, request *pbuf.Status) (*pbuf.Stat
 
 func getGyroValue() (float64, error) {
 	gyro1, err := ev3dev.SensorFor("ev3-ports:in1", "lego-ev3-gyro")
-	if err != nil {
+
+	gyro2, err2 := ev3dev.SensorFor("ev3-ports:in4", "lego-ev3-gyro")
+	if err != nil && err2 != nil {
 		return 0, err
-	}
-	gyro2, err := ev3dev.SensorFor("ev3-ports:in4", "lego-ev3-gyro")
-	if err != nil {
-		return 0, err
+	} else if err != nil {
+		tmp, _ := gyro2.Value(0)
+		pos2, _ := strconv.ParseFloat(tmp, 32)
+		return pos2, nil
+	} else if err2 != nil {
+		tmp, _ := gyro1.Value(0)
+		pos1, _ := strconv.ParseFloat(tmp, 32)
+		return pos1, nil
 	}
 	tmp, _ := gyro1.Value(0)
 	pos1, _ := strconv.ParseFloat(tmp, 32)
@@ -250,12 +256,46 @@ func getGyroValue() (float64, error) {
 	return (pos1 + pos2) / 2.0, nil
 }
 func resetGyros() {
-	gyro1, err := ev3dev.SensorFor("ev3-ports:in1", "lego-ev3-gyro")
-	if err != nil {
+	gyro1, err1 := ev3dev.SensorFor("ev3-ports:in1", "lego-ev3-gyro")
+	gyro2, err2 := ev3dev.SensorFor("ev3-ports:in4", "lego-ev3-gyro")
+
+	if err1 != nil && err2 != nil {
 		return
-	}
-	gyro2, err := ev3dev.SensorFor("ev3-ports:in4", "lego-ev3-gyro")
-	if err != nil {
+	} else if err1 != nil {
+		gyro2.SetMode("GYRO-ANG")
+		direct, err := gyro2.Direct(666)
+		if err != nil {
+			fmt.Printf("Gyro 2 open: %s\n", err)
+			return
+		}
+		_, err = direct.Write([]byte("\x88"))
+		if err != nil {
+			fmt.Printf("Gyro 2 write: %s\n", err)
+			return
+		}
+		err = direct.Close()
+		if err != nil {
+			fmt.Printf("Gyro 2 close: %s\n", err)
+			return
+		}
+		return
+	} else if err2 != nil {
+		gyro1.SetMode("GYRO-ANG")
+		direct, err := gyro1.Direct(777)
+		if err != nil {
+			fmt.Printf("Gyro 1 open: %s\n", err)
+			return
+		}
+		_, err = direct.Write([]byte("\x88"))
+		if err != nil {
+			fmt.Printf("Gyro 1 write: %s\n", err)
+			return
+		}
+		err = direct.Close()
+		if err != nil {
+			fmt.Printf("Gyro 1 close: %s\n", err)
+			return
+		}
 		return
 	}
 	gyro1.SetMode("GYRO-ANG")
