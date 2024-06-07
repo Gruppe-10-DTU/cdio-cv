48a3414a07957ebe915167379f1c33becec18112
andreasdan <andreas.petersen01@gmail.com>
2024-06-07 13:40:15 +0200

    Added functionality to detect line collision

---
+++ Pythoncode/Pathfinding/Clipping.py
@@ -0,0 +1,70 @@
+from Pythoncode.model.Rectangle import Rectangle
+from Pythoncode.model.coordinate import Coordinate
+
+"""
+This is an implementation of the Cohen-Sutherland algorithm for line clipping
+"""
+
+LEFT = 1
+RIGHT = 2
+BOTTOM = 4
+TOP = 8
+
+
+def calculate_endpoint_outcode(box: Rectangle, coordinate: Coordinate):
+    outcode = 0
+
+    if coordinate.x < box.c1.x:
+        outcode |= LEFT
+    elif coordinate.x > box.c2.x:
+        outcode |= RIGHT
+
+    if coordinate.y < box.c1.y:
+        outcode |= BOTTOM
+    elif coordinate.y > box.c2.y:
+        outcode |= TOP
+
+    return outcode
+
+
+def line_clips_rectangle(box: Rectangle, c1: Coordinate, c2: Coordinate):
+    start = calculate_endpoint_outcode(box, Coordinate(c1.x, c1.y))
+    end = calculate_endpoint_outcode(box, Coordinate(c2.x, c2.y))
+
+    does_clip = False
+    run = True
+
+    while run:
+        if not (start | end):
+            does_clip = True
+            run = False
+        elif start & end:
+            run = False
+        else:
+            outside = start if start > end else end
+            x = 0
+            y = 0
+
+            if outside & TOP:
+                x = c1.x + (c2.x - c1.x) * (box.c2.y - c1.y) / (c2.y - c1.y)
+                y = box.c2.y
+            elif outside & BOTTOM:
+                x = c1.x + (c2.x - c1.x) * (box.c1.y - c1.y) / (c2.y - c1.y)
+                y = box.c1.y
+            elif outside & LEFT:
+                y = c1.y + (c2.y - c1.y) * (box.c1.x - c1.x) / (c2.x - c1.x)
+                x = box.c1.x
+            elif outside & RIGHT:
+                y = c1.y + (c2.y - c1.y) * (box.c2.x - c1.x) / (c2.x - c1.x)
+                x = box.c2.x
+
+            if outside == start:
+                c1.x = x
+                c1.y = y
+                start = calculate_endpoint_outcode(box, Coordinate(c1.x, c1.y))
+            else:
+                c2.x = x
+                c2.y = y
+                end = calculate_endpoint_outcode(box, Coordinate(c2.x, c2.y))
+
+    return does_clip
---
+++ Pythoncode/tests/test_Clipping.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+
+from Pythoncode.model.Rectangle import Rectangle
+from Pythoncode.Pathfinding.Clipping import line_clips_rectangle
+from Pythoncode.model.coordinate import Coordinate
+
+
+class Test(TestCase):
+    def test_line_does_clip_corner_edge(self):
+        box = Rectangle(Coordinate(10, 10), Coordinate(20, 20))
+        x0 = 11
+        y0 = 9
+        x1 = 9
+        y1 = 11
+
+        clipped = line_clips_rectangle(box, Coordinate(x0, y0), Coordinate(x1, y1))
+
+        self.assertTrue(clipped)
+
+    def test_line_does_not_clip_corner_edge(self):
+        box = Rectangle(Coordinate(10, 10), Coordinate(20, 20))
+
+        x0 = 11
+        y0 = 8
+        x1 = 8
+        y1 = 11
+
+        clipped = line_clips_rectangle(box, Coordinate(x0, y0), Coordinate(x1, y1))
+
+        self.assertFalse(clipped)
+
+    def test_line_does_not_clip_outer(self):
+        box = Rectangle(Coordinate(10, 10), Coordinate(20, 20))
+
+        x0 = 1
+        y0 = 1
+        x1 = 4
+        y1 = 4
+
+        clipped = line_clips_rectangle(box, Coordinate(x0, y0), Coordinate(x1, y1))
+
+        self.assertFalse(clipped)
+
+    def test_line_does_clip_inside(self):
+        box = Rectangle(Coordinate(10, 10), Coordinate(20, 20))
+
+        x0 = 11
+        y0 = 11
+        x1 = 14
+        y1 = 14
+
+        clipped = line_clips_rectangle(box, Coordinate(x0, y0), Coordinate(x1, y1))
+
+        self.assertTrue(clipped)
