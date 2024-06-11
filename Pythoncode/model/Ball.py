726e48ac80c39578db20dce7568bbc4a2b16dbb4
Philip Cramer <s224319@dtu.dk>
2024-06-11 12:59:39 +0200

    created ball to wall distance function

--- Pythoncode/model/Ball.py
+++ Pythoncode/model/Ball.py
@@ -1,4 +1,5 @@
 from Pythoncode.model.coordinate import Coordinate
+from Pythoncode.Pathfinding import VectorUtils
 
 
 class Ball:
@@ -10,3 +11,10 @@ class Ball:
         self.y2 = y2
         self.id = id
         self.center = Coordinate(x1 + (x2-x1)/2, y1 + (y2-y1)/2)
+
+    def get_distance_to_wall(self, corner1, corner2):
+        wall = VectorUtils.get_vector(corner1.x1, corner1.y1, corner2.x1, corner2.y1)
+        corner_corner = VectorUtils.get_length(wall)
+        c1_ball = VectorUtils.get_vector(corner1.x1, corner1.y1, self.center.x, self.center.y)
+
+        return abs(c1_ball.get_dot_product(wall))/abs(corner_corner)
