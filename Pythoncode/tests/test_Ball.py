880bb3f3d612158f90192f181623ca56ed9f850e
Philip Cramer <s224319@dtu.dk>
2024-06-12 10:40:17 +0200

    created test for ball distance to wall method

--- Pythoncode/tests/test_Ball.py
+++ Pythoncode/tests/test_Ball.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from Pythoncode.model.Ball import Ball
+from Pythoncode.model.Corner import Corner
 
 
 class TestBall(TestCase):
@@ -9,4 +10,11 @@ class TestBall(TestCase):
         ball = Ball(0, 20, 20, 40, 1)
         self.assertEquals(ball.center.x, 10.0)
         self.assertEquals(ball.center.y, 30.0)
-
+    
+    def test_distance_to_wall(self):
+        ball = Ball(65, 35, , 55, 45, 1)
+        corner1 = Corner(5, 5, 15, 15)
+        corner2 = Corner(95, 5, 105, 15)
+        distance = ball.get_distance_to_wall(corner1, corner2)
+        self.assertEquals(distance, 30.0)
+        
