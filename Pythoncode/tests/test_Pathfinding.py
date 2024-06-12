e10a3e4e8e3e4102f223ed425aa0bd0732fc39ea
Asbjørn <s204026@student.dtu.dk>
2024-06-12 15:41:25 +0200

    Should avoid obstructed balls

--- Pythoncode/tests/test_Pathfinding.py
+++ Pythoncode/tests/test_Pathfinding.py
@@ -8,22 +8,20 @@ from Pythoncode.model.coordinate import Coordinate
 
 
 class TestPathfinding(TestCase):
-    """Just an outline for now. Have to wait for PAC to finish his code with drive_points so that I can test optimally."""
     def test_pathfind_with_obstacle_clipping(self):
         list_balls = [Ball(21,23 , 23, 29, 1), Ball(2,100,2,100,2),Ball(50, 10, 10, 70, 3)]
         start = Coordinate(1, 1)
-        drive_points = [Coordinate(45,45), Coordinate(100,100)]
         pathfinding = Pathfinding(targets=list_balls, start=start, obstacle=Rectangle(Coordinate(20, 20), Coordinate(30,30)))
 
-        ball = pathfinding.get_closest(start,drive_points)
+        ball = pathfinding.get_closest(start)
         self.assertEqual(list_balls[1].center.x,ball.center.x)
         list_balls.pop()
         self.assertEqual(len(list_balls),2)
-        ball = pathfinding.get_closest(start,drive_points)
+        ball = pathfinding.get_closest(start)
         self.assertEqual(list_balls[1].center.x, ball.center.x)
         list_balls.pop()
-        ball = pathfinding.get_closest(start,drive_points)
-        self.assertEqual(drive_points.pop(),ball)
+        ball = pathfinding.get_closest(start)
+        self.assertTrue(ball is None)
 
 
     def test_pathfind(self):
