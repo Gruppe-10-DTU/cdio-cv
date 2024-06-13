b68e517c19e96a0d7c6ece338ed459ef103683f5
sand4570 <s224323@student.dtu.dk>
2024-06-13 11:38:23 +0200

    Egg model with buffer

--- Pythoncode/model/Egg.py
+++ Pythoncode/model/Egg.py
@@ -10,27 +10,35 @@ class Egg:
         self.x2 = x2
         self.y2 = y2
         self.center = Coordinate(x1 + (x2 - x1) / 2, y1 + (y2 - y1) / 2)
+        self.buffer_center = None
+        self.buffer_c1 = None
+        self.buffer_c2 = None
+
+        buffer_dist = 4
+        buffer_radius = 3
+
+        direction = get_vector(rectangle.center,self.center)
+
+        if direction.x == 0:
+            if direction.y < 0:
+                self.buffer_center = Coordinate(self.center.x, self.center.y - buffer_dist)
+            else:
+                self.buffer_center = Coordinate(self.center.x, self.center.y + buffer_dist)
+        elif direction.x < 0 :
+            if direction.y == 0:
+                self.buffer_center = Coordinate(self.center.x - buffer_dist, self.center.y)
+            elif direction.y < 0:
+                self.buffer_center = Coordinate(self.center.x - buffer_dist, self.center.y - buffer_dist)
+            else:
+                self.buffer_center = Coordinate(self.center.x - buffer_dist, self.center.y + buffer_dist)
+        else:
+            if direction.y == 0:
+                self.buffer_center = Coordinate(self.center.x + buffer_dist, self.center.y)
+            elif direction.y < 0:
+                self.buffer_center = Coordinate(self.center.x + buffer_dist, self.center.y - buffer_dist)
+            else:
+                self.buffer_center = Coordinate(self.center.x + buffer_dist, self.center.y + buffer_dist)
 
 
-        distances = {"c1": (get_vector_length(get_vector(self.center, rectangle.c1))),
-                     "c2": (get_vector_length(get_vector(self.center, rectangle.c2))),
-                     "c3": (get_vector_length(get_vector(self.center, rectangle.c3))),
-                     "c4": (get_vector_length(get_vector(self.center, rectangle.c4)))}
-
-        sorted_distances = sorted(distances.items(), key=lambda x: x[1])
-        distance_array = []
-        i = 0
-        for x in sorted_distances.keys():
-            distance_array[i] = x
-            var = i + 1
-
-        self.buffer1 = None
-        self.buffer2 = None
-        if distance_array[0] == "c1"| "c3" & distance_array[1] == "c1"| "c3":
-            self.buffer1 = c1
-            self.buffer2 = distance_array[1]+7
-        elif distance_array[0] == "c1"| "c4" & distance_array[1] == "c1"| "c4":
-            self.buffer1 = distance_array[0]-7
-            self.buffer2 = distance_array[1]+7
-
-
+        self.buffer_c1 = Coordinate(self.buffer_center.x - buffer_radius, self.buffer_center.y + buffer_radius)
+        self.buffer_c2 = Coordinate(self.buffer_center.x + buffer_radius, self.buffer_center.y - buffer_radius)
