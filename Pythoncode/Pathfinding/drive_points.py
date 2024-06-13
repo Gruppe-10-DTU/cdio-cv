ad31d1861c331b2fee901b60978bd0cacad09f5b
Philip Cramer <s224319@dtu.dk>
2024-06-14 00:23:44 +0200

    updated drive point generator method

--- Pythoncode/Pathfinding/drive_points.py
+++ Pythoncode/Pathfinding/drive_points.py
@@ -1,51 +1,53 @@
 from Pythoncode.model.coordinate import Coordinate
-from Pythoncode.model.Corner import Corner, Placement
+from Pythoncode.model.Corner import Placement
 from Pythoncode.model.Vector import Vector
 from Pythoncode.Pathfinding import CornerUtils
 
-PRECICION = 5
-WALL_DISTANCE = 25
+PRECICION = 5 # drive point tolereance
+WALL_DISTANCE = 20 # wall clearance
 
 class drive_points:
-    def __init__(self, corners):
-        self.corners = CornerUtils.set_placements(corners)
+    def __init__(self, corners, mapscale):
+        self.corners = sorted(corners, key=lambda corner: corner.placement.value)
+        self.scale = mapscale
         self.drive_points = []
+        self.drive_points = self.__generate_drive_points()
         self.last = None
-        self.scale = CornerUtils.get_cm_per_pixel(corners)
-        self.generate_drive_points()
+        #generate_drive_points()
 
     def __generate_drive_points(self):
         left_wall = Vector(self.corners[0].center, self.corners[1].center)
         top_wall = Vector(self.corners[0].center, self.corners[2].center)
         right_wall = Vector(self.corners[2].center, self.corners[3].center)
         bottom_wall = Vector(self.corners[1].center, self.corners[3].center)
-        
+        drive_points = []
         for corner in self.corners:
             match corner.placement:
                 case Placement.TOP_LEFT:
-                    top_left = self.calculate_corner_drive_point(corner, top_wall, left_wall)
-                    self.drive_points.append(top_left)
-                    top_center = self.calculate_center_drive_point(top_left, top_wall)
-                    self.drive_points.append(top_center)
+                    top_left = self.__calculate_corner_drive_point(corner, top_wall, left_wall)
+                    drive_points.append(top_left)
+                    top_center = self.__calculate_center_drive_point(top_left, top_wall)
+                    drive_points.append(top_center)
                     break
                 case Placement.TOP_RIGHT:
-                    top_right = self.calculate_corner_drive_point(corner, top_wall.invert(), right_wall)
-                    self.drive_points.append(top_right)
-                    right_center = self.calculate_center_drive_point(top_right, right_wall)
-                    self.drive_points.append(right_center)
+                    top_right = self.__calculate_corner_drive_point(corner, top_wall.invert(), right_wall)
+                    drive_points.append(top_right)
+                    right_center = self.__calculate_center_drive_point(top_right, right_wall)
+                    drive_points.append(right_center)
                     break
                 case Placement.BOTTOM_LEFT:
-                    bottom_left = self.calculate_corner_drive_point(corner, left_wall.invert(), bottom_wall)
-                    self.drive_points.append(bottom_left)
-                    left_center = self.calculate_center_drive_point(bottom_left, left_wall.invert())
-                    self.drive_points.append(left_center)
+                    bottom_left = self.__calculate_corner_drive_point(corner, left_wall.invert(), bottom_wall)
+                    drive_points.append(bottom_left)
+                    left_center = self.__calculate_center_drive_point(bottom_left, left_wall.invert())
+                    drive_points.append(left_center)
                     break
                 case Placement.BOTTOM_RIGHT:
-                    bottom_right = self.calculate_corner_drive_point(corner, right_wall.invert(), bottom_wall.invert())
-                    self.drive_points.append(bottom_right)
-                    bottom_center = self.calculate_center_drive_point(bottom_right, bottom_wall.invert())
-                    self.drive_points.append(bottom_center)
+                    bottom_right = self.__calculate_corner_drive_point(corner, right_wall.invert(), bottom_wall.invert())
+                    drive_points.append(bottom_right)
+                    bottom_center = self.__calculate_center_drive_point(bottom_right, bottom_wall.invert())
+                    drive_points.append(bottom_center)
                     break
+        return self.drive_points
 
         
     def get_drive_points(self):
@@ -69,7 +71,7 @@ class drive_points:
         end = self.get_closest_drive_point(point)
         return Vector(end.x - point.x, end.y - point.y)
 
-    def __calculate_corner_drive_point(self,corner, wall1, wall2) -> Coordinate:
+    def __calculate_corner_drive_point(self, corner, wall1, wall2) -> Coordinate:
         vec1 = wall1.scale(WALL_DISTANCE * self.scale / wall1.length())
         vec2 = wall2.scale( WALL_DISTANCE * self.scale / wall2.length())
         return corner.center.add_vector(vec1.add(vec2))
