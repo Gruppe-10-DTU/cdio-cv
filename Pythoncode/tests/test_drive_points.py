205ad7dd4470db6c9d78283b2cd979cd99b5ab90
Philip Cramer <s224319@dtu.dk>
2024-06-14 10:31:48 +0200

    updated test cases

--- Pythoncode/tests/test_drive_points.py
+++ Pythoncode/tests/test_drive_points.py
@@ -4,7 +4,7 @@ from Pythoncode.Pathfinding.drive_points import Drive_points
 from Pythoncode.model.Corner import Corner, Placement
 
 class Test(TestCase):
-    def test_drive_points(self):
+    def test_drive_points_generation(self):
         top_left = Corner(0, 0, 10, 10, 1)
         top_left.set_placement(Placement.TOP_LEFT)
         
@@ -19,20 +19,50 @@ class Test(TestCase):
 
         corners = [top_left, bottom_left, top_right, bottom_right]
         dp = Drive_points(corners, 1.0)
-        print(dp.get_drive_points()[0].x)
-        print(dp.get_drive_points()[0].y)
         self.assertEqual(len(dp.get_drive_points()), 8)
-        
+
+
+    def test_all_drive_points_on_course(self):
+        top_left = Corner(0, 0, 10, 10, 1)
+        top_left.set_placement(Placement.TOP_LEFT)
+
+        top_right = Corner(100, 0, 110, 10, 2)
+        top_right.set_placement(Placement.TOP_RIGHT)
+        bottom_left = Corner(0, 100, 10, 110, 3)
+        bottom_left.set_placement(Placement.BOTTOM_LEFT)
+        bottom_right = Corner(100, 100, 110, 110, 4)
+        bottom_right.set_placement(Placement.BOTTOM_RIGHT)
+        corners = [top_left, bottom_left, top_right, bottom_right]
+        dp = Drive_points(corners, 1.0)
+        self.assertEqual(len(dp.get_drive_points()), 8)
+        points = dp.get_drive_points()
+        for point in points:
+            self.assertLess(top_left.center.x, point.x)
+            self.assertLess(top_left.center.y, point.y)
+            self.assertGreater(bottom_right.center.x, point.x)
+            self.assertGreater(bottom_right.center.y, point.y)
+
+    def test_drive_points_return_circular_order(self):
+        top_left = Corner(0, 0, 10, 10, 1)
+        top_left.set_placement(Placement.TOP_LEFT)
+
+        top_right = Corner(100, 0, 110, 10, 2)
+        top_right.set_placement(Placement.TOP_RIGHT)
+        bottom_left = Corner(0, 100, 10, 110, 3)
+        bottom_left.set_placement(Placement.BOTTOM_LEFT)
+        bottom_right = Corner(100, 100, 110, 110, 4)
+        bottom_right.set_placement(Placement.BOTTOM_RIGHT)
+        corners = [top_left, bottom_left, top_right, bottom_right]
+        dp = Drive_points(corners, 1.0)
+         
         start = dp.get_drive_points()[0]
-        end = start
+        current = start
+        previous = None
         for i in range(8):
-            end = dp.get_closest_drive_point(end)
+            next = dp.get_closest_drive_point(current)
+            self.assertNotEqual(previous, next)
+            previous = current
+            current = next
 
-            self.assertLess(top_left.center.x, end.x)
-            self.assertLess(top_left.center.y, end.y)
 
-            self.assertGreater(bottom_right.center.x, end.x)
-            self.assertGreater(bottom_right.center.y, end.y)
-
-        self.assertEqual(start, end)
-
+        self.assertEqual(start, current)
