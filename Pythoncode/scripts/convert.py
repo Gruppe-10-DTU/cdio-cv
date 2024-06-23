87b9592bed6e8a86e3ce34a1e610597c2bfe92d7
andreasdan <andreas.petersen01@gmail.com>
2024-06-23 21:39:30 +0200

    moved and refined scripts for training

--- Pythoncode/scripts/convert.py
+++ Pythoncode/scripts/convert.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
-model = YOLO("model/best.pt")
+model = YOLO("../model/best.pt")
 model.export(format="pt", half=True)
--- Pythoncode/scripts/training.py
+++ Pythoncode/scripts/training.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import torch
 
 # Model specific for training small objects
-model = YOLO("yolov8s-p2.yaml").load("yolov8s.pt")
+model = YOLO("yolov8s-p2.yaml").load("../yolov8s.pt")
 
 if __name__ == "__main__":
 
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     if torch.cuda.is_available():
         device = 0
 
-    model.train(data="data/data.yaml", epochs=100, imgsz=992, device=device, batch=-1)
+    model.train(data="data/data.yaml", epochs=100, imgsz=1120, device=device, batch=-1)
